4fca79764a644a4ae42c96d75c18980ce1884af4
Lili Stoliarchuk <lil2016st@gmail.com>
2024-07-19 21:02:18 +0200

    load tasks

--- tasks_folder/main.py
+++ tasks_folder/main.py
@@ -16,3 +16,25 @@ STATUSES = {
     COMPLETED: "завершенна"
 }
 DATA_FILE = 'tasks.txt'
+
+def load_tasks():
+    tasks = {}
+    try:
+        with open(DATA_FILE, 'r', encoding='utf-8') as file:
+            lines = file.readline()
+            for line in lines:
+                parts = line.strip().split('|')
+                if len(parts) == 5:
+                    task_id, name, description, priority, status = parts
+                    tasks[task_id] = {
+                        "name": name,
+                        "description": description,
+                        "priority": priority,
+                        "status": status
+                    }
+                else:
+                    print(f"Некорректная строка: {line.strip()}")
+    except FileExistsError:
+        print("Файл с задачами не найден")
+    return tasks
+
