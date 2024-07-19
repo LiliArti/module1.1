1c62b4c38b5ffe874213e53d1f96ec6116296987
Lili Stoliarchuk <lil2016st@gmail.com>
2024-07-19 21:01:01 +0200

    constants

---
+++ tasks_folder/main.py
@@ -0,0 +1,18 @@
+LOW = "1"
+MEDIUM = "2"
+HIGH = "3"
+PRIORITIES = {
+    LOW: "низкий",
+    MEDIUM: "средний",
+    HIGH: "высокий"
+}
+
+NEW = "1"
+IN_PROGRESS = "2"
+COMPLETED = "3"
+STATUSES = {
+    NEW: "новая",
+    IN_PROGRESS: "в процессе",
+    COMPLETED: "завершенна"
+}
+DATA_FILE = 'tasks.txt'
